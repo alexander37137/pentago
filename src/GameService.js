dcf9d6763c0bf8c6e81dd831d7f400e4db88240b
Alexander Andreev <alexander.andreev@restream.rt.ru>
2018-04-18 00:38:41 +0400

    col winner

--- src/GameService.js
+++ src/GameService.js
@@ -4,7 +4,7 @@ const ROW_NUMBER = 6;
 const COL_NUMBER = 6;
 
 export default {
-  canSetValue(pentago, row, cell, value) {
+  canSetValue(pentago, row, cell) {
     const val = path([row, cell], pentago);
     return val === 0;
   },
@@ -17,21 +17,64 @@ export default {
     return player === 1 ? 2 : 1;
   },
 
-  findWinner(pentago) {
-    for (var i = 0; i < ROW_NUMBER; i++) {
-      let currentPlayer = pentago[i][0];
+  findPentagoWinner(pentago) {
+    const rowIter = this.iterateRow(pentago);
+    const rowWinner = this.findWinner(pentago, rowIter);
+    if (rowWinner) {
+      return rowWinner;
+    }
+    const colIter = this.iterateCol(pentago);
+    const colWinner = this.findWinner(pentago, colIter);
+    return colWinner;
+  },
+
+  findWinner(pentago, iterator) {
+    let nextRow = iterator.next();
+    while (!nextRow.done) {
+      const cellIter = nextRow.value;
+
+      let nextCell = cellIter.next();
+      let currentPlayer = nextCell.value;
       let currentStack = 0;
-      for (var j = 0; j < COL_NUMBER; j++) {
-        if (currentPlayer === pentago[i][j]) {
+      while (!nextCell.done) {
+        if (currentPlayer === nextCell.value) {
           currentStack += 1;
         } else {
-          currentPlayer = pentago[i][j];
+          currentPlayer = nextCell.value;
           currentStack = 1;
         }
-        if (currentPlayer && currentStack == 5) {
+        if (currentPlayer && currentStack === 5) {
           return currentPlayer;
         }
+        nextCell = cellIter.next();
       }
+      nextRow = iterator.next();
+    }
+
+    return null;
+  },
+
+  *iterateCol(pentago) {
+    const iterate = function* iterate(pent, row) {
+      for (let i = 0; i < COL_NUMBER; i += 1) {
+        yield pent[i][row];
+      }
+    };
+
+    for (let i = 0; i < ROW_NUMBER; i += 1) {
+      yield iterate(pentago, i);
+    }
+  },
+
+  *iterateRow(pentago) {
+    const iterate = function* iterate(pent, col) {
+      for (let i = 0; i < ROW_NUMBER; i += 1) {
+        yield pent[col][i];
+      }
+    };
+
+    for (let i = 0; i < COL_NUMBER; i += 1) {
+      yield iterate(pentago, i);
     }
   },
 };
